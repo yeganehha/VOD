00cb084640d4ea9e5231c06dfa3045a668f391c5
Erfan Ebrahimi <me@erfanebrahimi.ir>
2025-06-24 14:17:22 +0330

    fix single page

--- resources/views/pages/singleMovie.blade.php
+++ resources/views/pages/singleMovie.blade.php
@@ -48,17 +48,17 @@
                                         @endforeach
                                     </div>
                                     @endif
-                                    <div class="movie-info-item">
-                                        <strong>کارگردان:</strong>
-                                        <a href="#">نیما نوبخت</a>
-                                    </div>
-                                    <div class="movie-info-item">
-                                        <strong>بازیگران:</strong>
-                                        <a href="#">دیکاپریو,</a>
-                                        <a href="#">تام کروز,</a>
-                                        <a href="#">آنجلینا جولی,</a>
-                                        <a href="#">آنتونی روبرت</a>
-                                    </div>
+{{--                                    <div class="movie-info-item">--}}
+{{--                                        <strong>کارگردان:</strong>--}}
+{{--                                        <a href="#">نیما نوبخت</a>--}}
+{{--                                    </div>--}}
+{{--                                    <div class="movie-info-item">--}}
+{{--                                        <strong>بازیگران:</strong>--}}
+{{--                                        <a href="#">دیکاپریو,</a>--}}
+{{--                                        <a href="#">تام کروز,</a>--}}
+{{--                                        <a href="#">آنجلینا جولی,</a>--}}
+{{--                                        <a href="#">آنتونی روبرت</a>--}}
+{{--                                    </div>--}}
                                     @if(count($movie->entity->countries) > 0 )
                                         <div class="movie-info-item">
                                             <strong>کشور:</strong>
@@ -75,10 +75,6 @@
                                         <strong>مدت زمان:</strong>
                                         <span>{{ $movie->durationTitle() }}</span>
                                     </div>
-                                    <div class="movie-info-item">
-                                        <strong>کیفیت:</strong>
-                                        <span class="quality">اچ دی</span>
-                                    </div>
                                     @if($movie->pro_year ?? $movie->entity->pro_year)
                                     <div class="movie-info-item">
                                         <strong>سال تولید:</strong>
@@ -90,6 +86,25 @@
                                         <strong>ای ام دی بی:</strong>
                                         <span class="rating">{{ $movie->imdb_rate }}</span>
                                     </div>
+                                    @endif
+                                </div>
+                                <div class="col-md-12">
+                                    @if(($movie->main_audio ?? $movie->entity->main_audio) or (! in_array($movie->main_audio ?? $movie->entity->main_audio , [\App\Enums\Audio::Persian]) and $movie->dubbed ) or $movie->is_multi_audio or $movie->has_subtitle )
+                                        <div class="movie-info-item">
+                                            <strong>زبان:</strong>
+                                            @if($movie->main_audio ?? $movie->entity->main_audio)
+                                            <span class="quality1">{{ $movie->main_audio?->getLabel() ?? $movie->entity->main_audio?->getLabel() }}</span>
+                                            @endif
+                                            @if( ! in_array($movie->main_audio ?? $movie->entity->main_audio , [\App\Enums\Audio::Persian]) and $movie->dubbed )
+                                                <span class="quality">دوبله شده</span>
+                                            @endif
+                                            @if( $movie->is_multi_audio )
+                                                <span class="quality1">چند زبانه</span>
+                                            @endif
+                                            @if( $movie->has_subtitle )
+                                                <span class="quality1">همراه با زیر نویس</span>
+                                            @endif
+                                        </div>
                                     @endif
                                 </div>
                             </div>
@@ -123,6 +138,51 @@
                     </div>
                 </div>
             </div>
+
+            @if(in_array($movie->entity->type , collect(\App\Enums\EntityType::cases())->filter(fn($item) => $item->isMultiEpisode())->toArray() ) )
+            <div class="season-wrap">
+                @if($movie->entity->type == \App\Enums\EntityType::MultiSeasonSeries)
+                <ul class="nav nav-pills" id="season-pills-tab" role="tablist">
+                    @for($season = 1 ; $season <= $movie->entity->movies()->max('season') ; $season++)
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link {{ $movie->season == $season ? 'active' : '' }}" id="pills-season-tab{{$season}}" data-bs-toggle="pill"
+                                data-bs-target="#pills-season{{$season}}" type="button" role="tab"
+                                aria-controls="pills-season{{$season}}" aria-selected="{{ $movie->season == $season ? 'true' : 'false' }}">فصل {{ $season }}</button>
+                    </li>
+                    @endfor
+                </ul>
+                @endif
+                <div class="tab-content" id="season-pills-tabContent">
+                    @for($season = 1 ; $season <= $movie->entity->movies()->max('season') ; $season++)
+                    <div class="tab-pane fade {{ $movie->season == $season ? 'show active' : '' }}" id="pills-season1" role="tabpanel"
+                         aria-labelledby="pills-season-tab{{$season}}" tabindex="{{$season - 1}}">
+                        <div class="season-content">
+                            <div class="row">
+
+                                @foreach($movie->entity->movies()->where('publish_date' , '<=', now()->addDays(7))->whereHas('entity' , fn($builder) => $builder->where('publish_status' , \App\Enums\PublishStatus::Publish->value))->where('season' , $season)->orderBy('episode')->get() as $episode)
+                                <div class="col-md-6 col-lg-4 col-xl-3">
+                                    <div class="movie-item">
+                                        <div class="movie-img">
+                                            <img style="width: 100%;min-height: 170px" data-movieId="{{ $episode->id }}" src="https://storage.googleapis.com/proudcity/mebanenc/uploads/2021/03/placeholder-image-300x225.png" alt="قسمت
+                                                {{ $episode->episode }} {{ $episode->title }}">
+                                            <a href="{{ route('movie.short' , $episode->id ) }}" class="movie-play"><i class="icon-play-4"></i></a>
+                                        </div>
+                                        <div class="movie-content">
+                                            <h6 class="movie-title"><a href="{{ route('movie.short' , $episode->id ) }}">قسمت
+                                                    {{ $episode->episode }} {{ $episode->season }} {{ $episode->title }}</a></h6>
+                                            <div class="movie-info">
+                                                <span class="movie-season">۱{{ $episode->durationTitle() }}</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                    @endfor
+            </div>
+            @endif
 
         </div>
     </div>
